a6d53889863b91ddc84352859a1f6d41d3f67924
Ali Moghimi <mghmiali@gmail.com>
2022-09-11 22:28:29 +0000

    add model admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from core import models
+
+
+class TaskAdmin(admin.TabularInline):
+    model = models.Task
+    extra = 1
+
+
+@admin.register(models.Project)
+class ProjectAdmin(admin.ModelAdmin):
+    ordering = ['-created_at']
+    search_fields = ['title', 'description']
+    list_display = ['title', 'owner', 'is_active']
+    list_filter = ['is_active', 'created_at', 'updated_at']
+    list_per_page = 10
+    readonly_fields = ['updated_at', 'created_at']
+    inlines = [TaskAdmin]
+    
