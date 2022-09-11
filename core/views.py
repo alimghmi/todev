ac4c8f9a1acbed19d70bc211cabe29de2dc7ed31
Ali Moghimi <mghmiali@gmail.com>
2022-09-11 22:15:34 +0000

    minor update on view

--- core/views.py
+++ core/views.py
@@ -1,11 +1,10 @@
 from django.db.models import Q
 from rest_framework import status
 from rest_framework import generics
-from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.permissions import (IsAuthenticated)
+from rest_framework.permissions import IsAuthenticated
 
 from core import models
 from core import serializers
