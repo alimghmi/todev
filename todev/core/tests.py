da035a2bd58cebab4638cb638436d56280fdc0d6
Ali Moghimi <mghmiali@gmail.com>
2022-09-12 15:28:30 +0000

    add test cases

--- todev/core/tests.py
+++ todev/core/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+from urllib import response
+from rest_framework.test import APITestCase
+from rest_framework import status
+from django.urls import reverse 
 
-# Create your tests here.
+
+class TestTaskAPI(APITestCase):
+
+    def create_not_work_for_anon(self):
+        data = {
+            "project": 1,
+            "title": "A new task",
+            "description": "",
+            "status": "T",
+            "assignees": []
+        }
+        response = self.client.post(reverse('create-task'), data)
+        self.assertEquals(response.status_code, status.HTTP_403_FORBIDDEN)
